c9f1cfdf6f69f25d9692d2f4a807e514f8594d12
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-20 15:38:48 +0900

    feat: 출력 포맷 맞추기 serializers 연습

--- VueDjAgency-ch0/api2/serializers.py
+++ VueDjAgency-ch0/api2/serializers.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Category, Tag
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,12 +12,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class PostListSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='category.name')
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'image', 'like', 'category']
 
 
 class PostRetrieveSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    tags = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Post
         exclude = ['create_dt']
@@ -29,7 +34,48 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class PostLikeSerializer(serializers.ModelSerializer):
+# class PostLikeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Post
+#         fields = ['like']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['name']
+
+
+# class CateTagSerializer(serializers.Serializer):
+#     cateList = CategorySerializer(many=True)
+#     tagList = TagSerializer(many=True)
+
+
+class CateTagSerializer(serializers.Serializer):
+    cateList = serializers.ListField(child=serializers.CharField())
+    tagList = serializers.ListField(child=serializers.CharField())
+
+
+class PostSerializerSub(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['like']
+        fields = ['id', 'title']
+
+
+class CommentSerializerSub(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'update_dt']
+
+
+class PostSerializerDetail(serializers.Serializer):
+    post = PostRetrieveSerializer()
+    prevPost = PostSerializerSub()
+    nextPost = PostSerializerSub()
+    commentList = CommentSerializerSub(many=True)
--- VueDjAgency-ch0/api2/views.py
+++ VueDjAgency-ch0/api2/views.py
@@ -1,4 +1,6 @@
 # from django.contrib.auth.models import User
+from collections import OrderedDict
+
 from rest_framework import viewsets
 #
 # from api2.serializers import UserSerializer, PostSerializer, CommentSerializer
@@ -18,21 +20,24 @@ from rest_framework import viewsets
 # class CommentViewSet(viewsets.ModelViewSet):
 #     queryset = Comment.objects.all()
 #     serializer_class = CommentSerializer
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, GenericAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer, PostLikeSerializer
-from blog.models import Post, Comment
-
-
-class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
+from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer, \
+    CateTagSerializer, PostSerializerDetail
+from blog.models import Post, Comment, Category, Tag
 
 
-class PostRetrieveAPIView(RetrieveAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostRetrieveSerializer
+# class PostListAPIView(ListAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostListSerializer
+
+
+# class PostRetrieveAPIView(RetrieveAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostRetrieveSerializer
 
 
 class CommentCreateAPIView(CreateAPIView):
@@ -40,23 +45,120 @@ class CommentCreateAPIView(CreateAPIView):
     serializer_class = CommentSerializer
 
 
-class PostLikeAPIView(UpdateAPIView):
+# class PostLikeAPIView(UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostLikeSerializer
+#
+#     # PATCH method
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         data = {'like': instance.like + 1}
+#         serializer = self.get_serializer(instance, data=data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+#
+#         if getattr(instance, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, we need to
+#             # forcibly invalidate the prefetch cache on the instance.
+#             instance._prefetched_objects_cache = {}
+#
+#         return Response(data['like'])
+
+
+class PostLikeAPIView(GenericAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostLikeSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    # GET method
+    def get(self, request, *args, **kwargs):
         instance = self.get_object()
-        data = {'like': instance.like + 1}
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        instance.like += 1
+        instance.save()
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(data['like'])
+        return Response(instance.like)
 
 
+class CateTagAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        cateList = Category.objects.all()
+        tagList = Tag.objects.all()
+
+        data = {
+            'cateList': cateList,
+            'tagList': tagList,
+        }
+
+        serializer = CateTagSerializer(instance=data)
+        return Response(serializer.data)
+
+
+class PostPageNumberPagination(PageNumberPagination):
+    page_size = 3
+
+    # page_size_query_param = 'page_size'
+    # max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('postList', data),
+            ('pageCnt', self.page.paginator.num_pages),
+            ('curPage', self.page.number),
+        ]))
+
+
+class PostListAPIView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    pagination_class = PostPageNumberPagination
+
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': None,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+
+def get_prev_next(instance):
+    try:
+        prev = instance.get_previous_by_update_dt()
+    except instance.DoesNotExist:
+        prev = None
+
+    try:
+        next_ = instance.get_next_by_update_dt()
+    except instance.DoesNotExist:
+        next_ = None
+
+    return prev, next_
+
+
+class PostRetrieveAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializerDetail
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        prevInstance, nextInstance = get_prev_next(instance)
+        commentList = instance.comment_set.all()
+        data = {
+            'post': instance,
+            'prevPost': prevInstance,
+            'nextPost': nextInstance,
+            'commentList': commentList,
+        }
+        serializer = self.get_serializer(instance=data)
+        return Response(serializer.data)
+
+    def get_serializer_context(self):
+        """
+        Extra context provided to the serializer class.
+        """
+        return {
+            'request': None,
+            'format': self.format_kwarg,
+            'view': self
+        }
