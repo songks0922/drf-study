647c71b5ca0c65260794f7d3f9d79820dba6eac2
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-07-20 13:14:48 +0900

    feat: drf 좋아요 API 만들기

--- VueDjAgency-ch0/api2/serializers.py
+++ VueDjAgency-ch0/api2/serializers.py
@@ -11,13 +11,25 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'username', 'email', 'is_staff']
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'image', 'like', 'category']
+
+
+class PostRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        exclude = ['create_dt']
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class PostLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['like']
